a96e7413f829930ab745e006444ca14e279a73bc
divya pateriya <divyapateriya14@outlook.com>
2022-10-27 21:48:20 +0530

    Added manimize, forward, backward methods.

--- SelemiumProject.py
+++ SelemiumProject.py
@@ -5,15 +5,33 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 #Give path to the browser driver
 service_obj = Service("C:/Users/divya/Downloads/chromedriver_win32/chromedriver.exe")
+
 #DriverObj is intermediation bw our system and chrome
 driver = webdriver.Chrome(service=service_obj) 
+
 #all actions to be done through the driver obj
 driver.get("https://www.youtube.com/") 
+
 #maximize the window
 driver.maximize_window()
+
 # get the title of the url
 print(driver.title)
+
+# minimize the window
+driver.minimize_window()
+
+# Go to a new url
+driver.get("https://www.youtube.com/channel/UC6tweGHw_8xAynq8ASYjkQQ")
+
+#Go back
+driver.back()
+
+#Go forward a page
+driver.forward()
+
 # make sure we landed to the right url before testing by checking curr url
 print(driver.current_url) 
+
 #close after everything is done
 driver.close()
