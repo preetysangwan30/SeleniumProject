e6d8e017bfda1006e70713d60775dfcfdf86c865
divya pateriya <divyapateriya14@outlook.com>
2022-11-01 23:27:18 +0530

    Introduced Dynamic dropdowns

---
+++ DynamicDropdowns.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+
+#Give path to the browser driver
+service_obj = Service("C:/Users/divya/Downloads/chromedriver_win32/chromedriver.exe")
+
+#If the browser closes automatically, below is how to sustain
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+#DriverObj is intermediation bw our system and chrome
+driver = webdriver.Chrome(service=service_obj)
+
+driver.get("https://rahulshettyacademy.com/dropdownsPractise/")
+
+#In the dynamic dropdown we'll send "ind" as an input and will wait till all the results show up
+driver.find_element(By.ID, "autosuggest").send_keys("ind")
+time.sleep(2)
+
+# find_elements return a list, hence will store into a var
+countries = driver.find_elements(By.CSS_SELECTOR, "li[class='ui-menu-item'] a")
+print(len(countries)) # printing the nos of outcomes
+
+# Looping through all the country elements and check if it is "India"
+for country in countries:
+    if country.text == "India":
+        country.click()
+        break
+
+
+
