f54ce0d043e4043516d521fca447f0dd65f1cb28
divya pateriya <divyapateriya14@outlook.com>
2022-12-16 23:13:43 +0530

    Introducing chaining --> ShoppingCart example

---
+++ WaitsDemo.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+
+#Give path to the browser driver
+service_obj = Service("C:/Users/divya/Downloads/chromedriver_win32/chromedriver.exe")
+
+#If the browser closes automatically, below is how to sustain
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+#DriverObj is intermediation bw our system and chrome
+driver = webdriver.Chrome(service=service_obj)
+driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
+driver.find_element(By.CSS_SELECTOR, ".search-keyword").send_keys("ber")
+time.sleep(2)
+
+# now we will write "ber" in the search bar and it will show matching products
+# here we will be writing the Xpath for the parent class and loop through
+# its child items
+
+products = driver.find_elements(By.XPATH, "//div[@class='products']/div")
+count = len(products)
+assert count > 0
+
+for product in products:
+    product.find_element(By.XPATH, "div/button").click()
+
+
