7b10521edab2665e6556c8e9e1d0887d1a9d8f18
divya pateriya <divyapateriya14@outlook.com>
2022-12-17 23:38:31 +0530

    Need to add the sleep() functions. Reason: That's issue with an element, it is loading first element right away but not all, thats why implicit or explicit wait won't work

--- WaitsDemo.py
+++ WaitsDemo.py
@@ -16,7 +16,7 @@ chrome_options.add_experimental_option("detach", True)
 #DriverObj is intermediation bw our system and chrome
 driver = webdriver.Chrome(service=service_obj)
 
-# driver.implicitly_wait(5)
+driver.implicitly_wait(5)
 # Global time out, throughout every code line if the content loads
 # selenium will wait max 5 secs for it to appear. If it appears before 5 secs
 # code will execute and save rest wait time
@@ -28,19 +28,20 @@ driver.find_element(By.CSS_SELECTOR, ".search-keyword").send_keys("ber")
 # now we will write "ber" in the search bar and it will show matching products
 # here we will be writing the Xpath for the parent class and loop through
 # its child items
+time.sleep(3)
 
 products = driver.find_elements(By.XPATH, "//div[@class='products']/div")
 count = len(products)
 assert count > 0
 
 for product in products:
-    element = WebDriverWait(driver,15).until(EC.element_to_be_clickable((By.XPATH, "div/button")))
-    element.click()
-    # product.find_element(By.XPATH, "div/button").click()
+    #element = WebDriverWait(driver,15).until(EC.element_to_be_clickable((By.XPATH, "div/button")))
+    # element.click()
+    product.find_element(By.XPATH, "div/button").click()
 
 driver.find_element(By.CSS_SELECTOR, ".cart-icon").click()
 driver.find_element(By.XPATH, "//button[text()='PROCEED TO CHECKOUT']").click()
-# time.sleep(3)
+time.sleep(3)
 driver.find_element(By.XPATH, "//input[@type='text']").send_keys("rahulshettyacademy")
 driver.find_element(By.CSS_SELECTOR, ".promoBtn").click()
 
