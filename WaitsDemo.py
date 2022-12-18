6a9af768d04dbb7950c95d9fc70a0ccd5c7c378e
divya pateriya <divyapateriya14@outlook.com>
2022-12-18 18:03:03 +0530

    Implicit waits have exception for find_elements() method. Also the components which takes further time after successful loading of the pages implicit do not work for them and explicit waits must come to rescue.

--- WaitsDemo.py
+++ WaitsDemo.py
@@ -2,8 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 import time
 
 #Give path to the browser driver
@@ -23,25 +21,24 @@ driver.implicitly_wait(5)
 
 driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
 driver.find_element(By.CSS_SELECTOR, ".search-keyword").send_keys("ber")
-# time.sleep(2)
 
 # now we will write "ber" in the search bar and it will show matching products
 # here we will be writing the Xpath for the parent class and loop through
 # its child items
-time.sleep(3)
+time.sleep(3) # Need to include sleep() even though we have an implicit wait is because
+# find_elements() is the only exception when implicit wait results into an error
+# because at first the list is empty means nothing has been displayed
 
 products = driver.find_elements(By.XPATH, "//div[@class='products']/div")
 count = len(products)
 assert count > 0
 
 for product in products:
-    #element = WebDriverWait(driver,15).until(EC.element_to_be_clickable((By.XPATH, "div/button")))
-    # element.click()
     product.find_element(By.XPATH, "div/button").click()
 
 driver.find_element(By.CSS_SELECTOR, ".cart-icon").click()
 driver.find_element(By.XPATH, "//button[text()='PROCEED TO CHECKOUT']").click()
-time.sleep(3)
+time.sleep(3) # will try to enhance this part in the ExplicitWaits.py
 driver.find_element(By.XPATH, "//input[@type='text']").send_keys("rahulshettyacademy")
 driver.find_element(By.CSS_SELECTOR, ".promoBtn").click()
 
