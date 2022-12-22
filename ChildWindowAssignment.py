673d6b38bac231f098a45fc6028bbbe6273b9ec9
divya pateriya <divyapateriya14@outlook.com>
2022-12-22 20:01:52 +0530

    Cleaned assignment with more understandable features

--- ChildWindowAssignment.py
+++ ChildWindowAssignment.py
@@ -18,7 +18,8 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(service=service_obj)
 driver.implicitly_wait(5)
 driver.get("https://rahulshettyacademy.com/loginpagePractise/")
-driver.find_element(By.LINK_TEXT, "Free Access to InterviewQues/ResumeAssistance/Material").click()
+# driver.find_element(By.LINK_TEXT, "Free Access to InterviewQues/ResumeAssistance/Material").click()
+driver.find_element(By.CLASS_NAME, "blinkingText").click()
 windows_opened = driver.window_handles
 driver.switch_to.window(windows_opened[1])
 email = driver.find_element(By.LINK_TEXT, "mentor@rahulshettyacademy.com").text
@@ -31,8 +32,8 @@ driver.find_element(By.ID, "username").send_keys(email)
 driver.find_element(By.ID, "password").send_keys("123456")
 driver.find_element(By.ID, "signInBtn").click()
 wait = WebDriverWait(driver,10)
-wait.until(expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, ".alert-danger")))
-print(driver.find_element(By.CSS_SELECTOR, ".alert-danger").text)
+wait.until(expected_conditions.visibility_of_element_located((By.XPATH, "//div[@class='alert alert-danger col-md-12']")))
+print(driver.find_element(By.XPATH, "//div[@class='alert alert-danger col-md-12']").text)
 
 
 
