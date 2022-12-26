bb1dc2f85363c9c2ac1a3189646f9e81932589f4
divya pateriya <divyapateriya14@outlook.com>
2022-12-26 19:21:18 +0530

    Handling frames

---
+++ iFrames.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+#Give path to the browser driver
+service_obj = Service("C:/Users/divya/Downloads/chromedriver_win32/chromedriver.exe")
+
+#If the browser closes automatically, below is how to sustain
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+#DriverObj is intermediation bw our system and chrome
+driver = webdriver.Chrome(service=service_obj)
+driver.implicitly_wait(5)
+
+driver.get("https://the-internet.herokuapp.com/iframe")
+driver.switch_to.frame("mce_0_ifr")
+driver.find_element(By.ID, "tinymce").clear()
+driver.find_element(By.ID, "tinymce").send_keys("Hello")
+driver.switch_to.default_content() # switching to defaultl html out of the frame , where we originall were
+print(driver.find_element(By.CSS_SELECTOR, "h3").text)
