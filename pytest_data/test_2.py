ce3184a56c72c6fc47fd54e1a6e2874ea777fffc
divya pateriya <divyapateriya14@outlook.com>
2022-12-28 19:42:03 +0530

    Starting with the pytest framework

---
+++ pytest_data/test_2.py
@@ -0,0 +1,4 @@
+def test_AssertCheck():
+	msg = "Hello"
+	assert msg == "Hi" , "Strings do not match" # Message when there is an assertion error
+
---
+++ pytest_data/test_helloWorld.py
@@ -0,0 +1,8 @@
+# The test file must start with test_
+# All of the code must be written inside the methods 
+# The method name should begin with test_ 
+
+def test_firstcode():
+    print("first pytest code")
+def test_greet():
+    print("Good Morning!!!")
