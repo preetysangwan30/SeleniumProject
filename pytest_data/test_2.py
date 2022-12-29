69f6d93215dcda0893168e7ad43a5ff32d4a0fce
divya pateriya <divyapateriya14@outlook.com>
2022-12-29 17:35:31 +0530

    Marks skip, xpass and groups introduced

--- pytest_data/test_2.py
+++ pytest_data/test_2.py
@@ -1,4 +1,12 @@
+import pytest
+
+@pytest.mark.Smoke
+@pytest.mark.skip
 def test_AssertCheck():
 	msg = "Hello"
 	assert msg == "Hi" , "Strings do not match" # Message when there is an assertion error
 
+
+@pytest.mark.xfail
+def test_excerpt():
+	print("An example for xfail")
--- pytest_data/test_helloWorld.py
+++ pytest_data/test_helloWorld.py
@@ -1,8 +1,13 @@
 # The test file must start with test_
 # All of the code must be written inside the methods 
 # The method name should begin with test_ 
+# Tests can be marked (tagged) as @pytest.mark.Smoke (Smoke is mark) and run with -m 
+
+import pytest
 
 def test_firstcode():
     print("first pytest code")
+
+@pytest.mark.Smoke # Smoke is like a category
 def test_greet():
     print("Good Morning!!!")
