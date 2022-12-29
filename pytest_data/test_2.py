f0255d33fc371510246d1b70c22b4d166d1ba266
divya pateriya <divyapateriya14@outlook.com>
2022-12-29 22:17:40 +0530

    Addition of fixtures

--- pytest_data/test_2.py
+++ pytest_data/test_2.py
@@ -6,7 +6,6 @@ def test_AssertCheck():
 	msg = "Hello"
 	assert msg == "Hi" , "Strings do not match" # Message when there is an assertion error
 
-
 @pytest.mark.xfail
 def test_excerpt():
 	print("An example for xfail")
