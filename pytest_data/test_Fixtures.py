515273f6c7350dfd381dea0607788745e70db135
divya pateriya <divyapateriya14@outlook.com>
2022-12-30 16:19:17 +0530

    Encanpsulated methods in the class with fixtures

--- pytest_data/test_Fixtures.py
+++ pytest_data/test_Fixtures.py
@@ -1,5 +1,22 @@
 # Here we will discuss fixtures, fixtures are nothing but the preresquisites
 # or programs that run post execution
 
-def test_fixtureDemo(scope):
-    print("I'll be executed second !!!") 
+import pytest
+# In case a fixture is applicable for many TCs, It's good to wrap them in a class
+# It's good to wrap them in a class and use a fixture as mentioned
+
+@pytest.mark.usefixtures("scope")
+
+class Test_example: # Class name must start with Test not test
+
+    def test_fixtureDemo(self):
+        print("I'll be executed second !!!") 
+
+    def test_fixtureDemo1(self):
+        print("I'll be executing in demo1 !!!") 
+
+    def test_fixtureDemo2(self):
+        print("I'll be executing in demo2 !!!") 
+
+    def test_fixtureDemo3(self):
+        print("I'll be executing in demo3 !!!") 
